309b6860da14ffbeca333aafdd640c749bb20b42
Christopher Cruzen <christopher.a.cruzen@gmail.com>
2023-03-12 00:30:00 -0600

    Add Helper TestingScripts

---
+++ Source/refresh_mcc_maps.bat
@@ -0,0 +1,6 @@
+
+@echo off
+
+rmdir /q /s "E:\Programming\Halo 2A Sound Restoration\Source\input\tags\sound_remastered\weapons"
+
+robocopy "D:\Games\Installed - Steam\steamapps\common\H2EK\h2_maps_win64_dx11" "D:\Games\Installed - Steam\steamapps\common\Halo The Master Chief Collection\halo2\h2_maps_win64_dx11" /E
