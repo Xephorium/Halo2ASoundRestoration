d9795bb58a27d032a34454025056f440ee731aad
Christopher Cruzen <christopher.a.cruzen@gmail.com>
2023-03-23 15:28:00 -0500

    Add Base Gain Values for The Oracle

--- Source/src/tags/levels/TheOracleTags.java
+++ Source/src/tags/levels/TheOracleTags.java
@@ -2,6 +2,7 @@ package tags.levels;
 
 import prefs.RestorationPreferences;
 import tags.TagGroup;
+import tags.TagModification;
 
 public class TheOracleTags extends TagGroup {
 
@@ -19,6 +20,39 @@ public class TheOracleTags extends TagGroup {
 
                 // Cable Elevator
                 "/sound_remastered/ambience/device_machines/control_lift",
+        };
+
+        tagModifications = new TagModification[]{
+
+                // Music
+                new TagModification(
+                        "/sound_remastered/scenarios/solo/04b_floodlab/04b_music/04b_01.sound_looping",
+                        prefs.getMusicGain() + -5 // Gain 0 from default
+                ),
+                new TagModification(
+                        "/sound_remastered/scenarios/solo/04b_floodlab/04b_music/04b_02.sound_looping",
+                        prefs.getMusicGain() + -3 // Gain 0 from default
+                ),
+                new TagModification(
+                        "/sound_remastered/scenarios/solo/04b_floodlab/04b_music/04b_04.sound_looping",
+                        prefs.getMusicGain() + -6 // Gain 0 from default
+                ),
+                new TagModification(
+                        "/sound_remastered/scenarios/solo/04b_floodlab/04b_music/04b_05.sound_looping",
+                        prefs.getMusicGain() + -9 // Gain 0 from default
+                ),
+                new TagModification(
+                        "/sound_remastered/scenarios/solo/04b_floodlab/04b_music/04b_06.sound_looping",
+                        prefs.getMusicGain() + -5 // Gain 0 from default
+                ),
+                new TagModification(
+                        "/sound_remastered/scenarios/solo/04b_floodlab/04b_music/04b_07.sound_looping",
+                        prefs.getMusicGain() + 0 // Gain 0 from default
+                ),
+                new TagModification(
+                        "/sound_remastered/scenarios/solo/04b_floodlab/04b_music/04b_08.sound_looping",
+                        prefs.getMusicGain() + -5 // Gain 0 from default
+                ),
         };
     }
 }
